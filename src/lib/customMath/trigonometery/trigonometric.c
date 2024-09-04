e68af85c8617f797857dfa6c0beb5df68697ee17
prana <pranavmudgil333@gmail.com>
2024-09-04 22:20:29 +0530

    Completed sine function, and added simplification conditions.

--- src/lib/customMath/trigonometery/trigonometric.c
+++ src/lib/customMath/trigonometery/trigonometric.c
@@ -3,32 +3,36 @@
 #include "../utilities/math_utils.h"
 #include "../exponents/exponents.h"
 
+/// @brief Calculates value of sine value using taylor series
+/// @param angle (double) intput angle
+/// @param is_rad (int) 1 if in radian and 0 if degree
+/// @return (double) sin(x)
 double sine(double angle, int is_rad) {
     //Taylor Series Expansion of Sine Function
     //sigma(0, infinity) = {[(-1)^(n)]/[(2n + 1)!]}*[x^(2n + 1)]
-    angle = (is_rad != 1) ? rad_to_deg(angle) : angle;
+    angle = (is_rad == 0) ? deg_to_rad(angle) : angle;  // Convert to radians, if needed
     if (absolute(angle) < APPROXIMATION_THRESHOLD) {
         return angle;
     }
     else {
-        //double rad_angle = (rad_angle > PI/2) ?: rad_angle/(2*PI) , rad_angle;
-        double term, buffer_sine_value = 0.0;
-    
-        // Normalize x to the range [0, 2π)
-        /*rad_angle = fmod(rad_angle, 2 * PI);
-        if (rad_angle < 0) {
-            rad_angle += 2 * PI;
-        }*/
-        
-        int i = 0;
-        do {
-            term = power(angle, 2 * i + 1) / factorial(2 * i + 1);
-            if (i % 2 == 1) {
-                term = -term;
-            }
+        if (absolute(angle) > PI / 2) {
+            // sin(x % 2PI) = sin(x) 
+            angle = mod(angle, 2*PI);
+        }
+        if (absolute(angle) > 2 * PI) {
+            // sin(x - 2PI) = sin(x)
+            int cycles = (int)(angle / (2*PI));
+            double simplification_factor = cycles * 2 * PI;
+            angle += (is_positive(angle) == 1) ? -simplification_factor : simplification_factor;
+        }
+        double term = angle; // First term of the series
+        double buffer_sine_value = term; // Intialize answer placeholder
+        int i = 1;
+        while (i < MAX_TERM_LIMIT && (term > TOLERANCE || term < -TOLERANCE)) {
+            term *= -angle * angle / ((2 * i) * ((2 * i) + 1));
             buffer_sine_value += term;
             i++;
-        } while (i < MAX_TERM_LIMIT && absolute(term) >= TOLERANCE);
+        }
     return buffer_sine_value;
     }
 }
