64e830c8183138eb6047239c52083bb322dbe9af
prana <pranavmudgil333@gmail.com>
2024-09-25 13:33:50 +0530

    Updated BIN file for new executables.

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ gcc -c src/lib/customMath/exponents/exponents.c -I./src/lib/customMath -o build/
 gcc -c src/lib/customMath/trigonometery/trigonometric.c -I./src/lib/customMath -o build/trigonometric.o
 gcc -c src/lib/customMath/utilities/math_utils.c -I./src/lib/customMath -o build/math_utils.o
 gcc -c src/lib/customMath/array_handling/sorter.c -I/src/lib/customMath -o build/sorter.o
-gcc -o build/main build/main.o build/exponents.o build/trigonometric.o build/math_utils.o build/sorter.o
+gcc -c src/lib/customMath/vectors/vector.c -I/src/lib/customMath -o build/vector.o
+gcc -o build/main build/main.o build/exponents.o build/trigonometric.o build/math_utils.o build/sorter.o build/vector.o
 echo Build complete!
 
