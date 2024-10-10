9724b64e28f9c6e611a81732b08ce88057e93dfe
Pranav Mudgil <pranavmudgil333@gmail.com>
2024-10-10 23:19:35 +0530

    Change the error handlink keys and made lookup static

--- src/lib/customMath/error_handling.h
+++ src/lib/customMath/error_handling.h
@@ -2,15 +2,20 @@
 #define ERROR_HANDLING_H
 
 // Error Codes
-#define ERR_MEM_ALLOC 'M' // Memory allocation error
-#define DIV_BY_ZERO 'Z' // Division by zero error
-#define COMPLEX_RES 'C' // The result is a compex number
+#define ERR_MEM_ALLOC 1 // Memory allocation error
+#define DIV_BY_ZERO 2   // Division by zero error
+#define COMPLEX_RES 3   // The result is a complex number
+#define POSITIVE_FUNC 4   // The result is a complex number
+
+// Declare the error_messages array in the .h file
+static const char* error_messages[];
 
 // Hash table with error messages
-const char* error_messages[] {
-    [ERR_MEM_ALLOC] = "Error occured during memory allocation",
-    [DIV_BY_ZERO] =   "Division y zero error occured",
-    [COMPLEX_RES] = "The result will be a complex number which is out of scope"
+static const char* error_messages[] = {
+    [ERR_MEM_ALLOC] = "Error occurred during memory allocation",
+    [DIV_BY_ZERO] = "Division by zero error occurred",
+    [COMPLEX_RES] = "The result will be a complex number which is out of scope",
+    [POSITIVE_FUNC] = "The mathmatical function accepts only non zero positive values"
 };
 
 #endif
