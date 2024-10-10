6ed9f92d429ca51136f04e605e3f229dd208e9fb
Pranav Mudgil <pranavmudgil333@gmail.com>
2024-10-10 23:17:53 +0530

    Added linked lists and error handling headers

--- src/lib/customMath/custom_math.h
+++ src/lib/customMath/custom_math.h
@@ -6,6 +6,8 @@
 #include "trigonometery/trigonometric.h"
 #include "utilities/math_utils.h"
 #include "array_handling/sorter.h"
+#include "array_handling/linked_lists/single.h"
 #include "vectors/vector.h"
+#include "error_handling.h"
 
 #endif // CUSTOM_MATH_H
