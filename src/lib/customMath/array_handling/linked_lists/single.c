6a4b45e89e3ab4b8a10044b60983a88e33e3a08e
Pranav Mudgil <pranavmudgil333@gmail.com>
2024-10-10 23:32:21 +0530

    Added error handling

--- src/lib/customMath/array_handling/linked_lists/single.c
+++ src/lib/customMath/array_handling/linked_lists/single.c
@@ -1,12 +1,17 @@
 #include "single.h"
+#include "../../error_handling.h"
 #include <stdlib.h>
 
 /// @brief Adds a element to the beggining of the list
 /// @param head (pointer to Node) The first element of the list, enter NULL if empty
 /// @param data (int)
 /// @return The new first element of the list
-SLL_Node *SLL_add_to_begginging(SLL_Node *head, int data) {
-    SLL_Node *new_head_node;
+SLL_Node *SLL_add_to_begginging(SLL_Node *head, int data, int *error_message) {
+    SLL_Node *new_head_node = (SLL_Node *)malloc(1 * sizeof(SLL_Node));
+    if (new_head_node == NULL) {
+        *error_message = ERR_MEM_ALLOC;
+        return head;
+    }
     new_head_node->data = data;
     new_head_node->next_node_ptr = head;
     return new_head_node;
@@ -16,15 +21,19 @@ SLL_Node *SLL_add_to_begginging(SLL_Node *head, int data) {
 /// @param head (pointer to Node) The first element of the list
 /// @param data (int)
 /// @return The first element of the list
-SLL_Node *SLL_append(SLL_Node *head, int data) {
+SLL_Node *SLL_append(SLL_Node *head, int data, int *error_message) {
     if (head == NULL) {
         // In case list is empty the last elemnt will just be the first element
-        return SLL_add_to_begginging(head, data);
+        return SLL_add_to_begginging(head, data, error_message);
     }
 
     SLL_Node *current_node = head;
-    SLL_Node *prev_node;
+    SLL_Node *prev_node = (SLL_Node *)malloc(1 * sizeof(SLL_Node));
     SLL_Node *new_end_node = (SLL_Node *)malloc(1 * sizeof(SLL_Node));
+    if ((prev_node == NULL) || (new_end_node == NULL)) {
+        *error_message = ERR_MEM_ALLOC;
+        return head;
+    }
     new_end_node->data = data;
 
     new_end_node->next_node_ptr = NULL;
@@ -39,18 +48,22 @@ SLL_Node *SLL_append(SLL_Node *head, int data) {
     return head;
 }
 
-/// @brief 
-/// @param head 
-/// @param index 
-/// @param data 
-/// @return 
-SLL_Node *SLL_insert(SLL_Node *head, int index, int data) {
+/// @brief Inserts a element at a given index
+/// @param head The first element of the list
+/// @param index The index at which the element will be inserted (starts from 0)
+/// @param data (int)
+/// @return The first element of the list
+SLL_Node *SLL_insert(SLL_Node *head, int index, int data, int *error_message) {
     if (index == 0) {
         // In case index is 0 the new element will become head
-        return SLL_add_to_begginging(head, data);
+        return SLL_add_to_begginging(head, data, error_message);
     }
 
     SLL_Node *new_node = (SLL_Node *)malloc(1 * sizeof(SLL_Node));
+    if (new_node == NULL) {
+        *error_message = ERR_MEM_ALLOC;
+        return head;
+    }
     new_node->data = data;
 
     SLL_Node *current_node = head;
@@ -63,7 +76,7 @@ SLL_Node *SLL_insert(SLL_Node *head, int index, int data) {
     }
 
     if (current_node == NULL) {
-        // The index is out of range
+        *error_message = INDEX_OUT_OF_RANGE;
         return head;
     }
 
