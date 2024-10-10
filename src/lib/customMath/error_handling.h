23d99121f2effa15c641b7464ddbd36f521a04dd
Pranav Mudgil <pranavmudgil333@gmail.com>
2024-10-10 23:31:38 +0530

    Added index error

--- src/lib/customMath/error_handling.h
+++ src/lib/customMath/error_handling.h
@@ -6,6 +6,7 @@
 #define DIV_BY_ZERO 2   // Division by zero error
 #define COMPLEX_RES 3   // The result is a complex number
 #define POSITIVE_FUNC 4   // The result is a complex number
+#define INDEX_OUT_OF_RANGE 5   // The index is out of range
 
 // Declare the error_messages array in the .h file
 static const char* error_messages[];
@@ -15,7 +16,8 @@ static const char* error_messages[] = {
     [ERR_MEM_ALLOC] = "Error occurred during memory allocation",
     [DIV_BY_ZERO] = "Division by zero error occurred",
     [COMPLEX_RES] = "The result will be a complex number which is out of scope",
-    [POSITIVE_FUNC] = "The mathmatical function accepts only non zero positive values"
+    [POSITIVE_FUNC] = "The mathmatical function accepts only non zero positive values",
+    [INDEX_OUT_OF_RANGE] = "The index is out of range"
 };
 
 #endif
